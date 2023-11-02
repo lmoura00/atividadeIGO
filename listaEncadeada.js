c96543b553bb599a189e84d11d8955632fb1ce3a
Lucas <lucasmoura53@gmail.com>
2023-11-02 12:13:03 -0300

    'lucas'

--- listaEncadeada.js
+++ listaEncadeada.js
@@ -7,6 +7,7 @@ module.exports = class LinkedList{
         this.count = 0
         this.head = undefined
         this.chamado = 0
+        this.chamadoTotal = 0
         this.senha = 1
     }
     senhaNormal(){
@@ -47,12 +48,49 @@ module.exports = class LinkedList{
     }
     chamarPaciente(){
         if(this.chamado === 2){
+            console.log("Chamando prioridade")
+            this.chamado = 0
+            this.chamadoTotal++
             //aqui vai chamar prioridade
             //ao findar de chamar o prioridade, reseta o chamado para 0, com isso volta a chamar o normal
         }
         else{
-            //aqui vai chamar normal
+            console.log("chamando normal")
+            this.chamado++
+            this.chamadoTotal++
             //lembrando que ele deve adionar um ao chamado toda vez q terminar de chamar uma senha 
+        }
+    }
+    pegarElemento(index){
+        if(index>=0 && index<this.count){
+            let no = this.head
+            for(let i =0 ; i< index && no != null;i++){
+                no = no.next
+            }
+            return no
+        }
+        else{
+            console.log("Index impossível!! Tente outro valor")
+        }
+    }
+    addOndeEuQuiser(num, index){
+        if(index>=0 && index<=this.count){
+            const no = new normal(num)
+            if(index === 0){
+                const atual = this.head
+                no.next = atual
+                this.head = no
+            }
+            else{
+                const antes = this.pegarElemento(index-1)
+                no.next = antes.next
+                antes.next = no
+            }
+            this.count++
+            console.log("O número ", num, ' foi adicionado com sucesso.')
+        }
+        else{
+            console.log("Index impossível!! Tente novamente")
         }
     }
    
